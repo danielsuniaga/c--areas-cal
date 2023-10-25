fe2e55c12e7d1fcede0524b61781ac3c7a57d214
Daniel Suniaga <danieldsuniaga@gmail.com>
2023-10-25 16:38:45 -0500

    first commit

---
+++ Codifico-Test-C#/Program.cs
@@ -0,0 +1,93 @@
+﻿using Codifico_Test_C_;
+using System;
+
+
+class Program
+{
+
+
+    static void Main()
+    {
+
+        //the class instance is generated
+        area_calculus management = new area_calculus();
+
+        while (true)
+        {
+
+            Console.WriteLine("Calculadora de Área de Figuras Geométricas");
+
+            Console.WriteLine("1. Calcular el área de un círculo");
+
+            Console.WriteLine("2. Calcular el área de un rectángulo");
+
+            Console.WriteLine("3. Salir");
+
+            string _option = Console.ReadLine();
+
+            if (_option == "1")
+            {
+
+                // The data is requested to calculate the area of the circle
+                Console.Write("Ingrese el radio del círculo: ");
+
+                if (double.TryParse(Console.ReadLine(), out double _radio) && _radio > 0)
+                {
+                    
+                    Console.WriteLine(management.circle(_radio));
+                   
+                }
+                else
+                {
+                    Console.WriteLine("El radio debe ser un número positivo.");
+                }
+            }
+            else if (_option == "2")
+            {
+                // The data is requested to calculate the area of the rectangle
+                Console.Write("Ingrese la base del rectángulo: ");
+
+                if (double.TryParse(Console.ReadLine(), out double rectangle_base) && rectangle_base > 0)
+                {
+
+                    Console.Write("Ingrese la altura del rectángulo: ");
+
+                    if (double.TryParse(Console.ReadLine(), out double rectangle_height) && rectangle_height > 0)
+                    {
+
+                        Console.WriteLine(management.rectangle(rectangle_base,rectangle_height));
+
+                    }
+                    else
+                    {
+
+                        Console.WriteLine("La altura debe ser un número positivo.");
+
+                    }
+
+                }
+                else
+                {
+
+                    Console.WriteLine("La base debe ser un número positivo.");
+
+                }
+            }
+            else if (_option == "3")
+            {
+
+                // exit the program
+                Console.WriteLine("¡Hasta luego!");
+
+                break;
+
+            }
+            else
+            {
+
+                Console.WriteLine("Opción no válida. Por favor, elija 1, 2 o 3.");
+
+            }
+        }
+    }
+}
---
+++ Codifico-Test-C#/area_calculus.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codifico_Test_C_
+{
+
+    /*class that will process all area calculations*/
+    public class area_calculus
+    {
+
+        /*class constructor*/
+        public area_calculus() { }
+
+        /*method that calculates the area of a circle*/
+        public string circle(double radio)
+        {
+
+            try
+            {
+
+                double areaCirculo = Math.PI * Math.Pow(radio, 2);
+
+                return $"El área del círculo con radio {radio} es: {areaCirculo}";
+
+            }
+            catch (Exception ex)
+            {
+
+                return $"Error al calcular el área del círculo: {ex.Message}";
+
+            }
+
+        }
+
+        /*method that calculates the area of a rectangle*/
+
+        public string rectangle(double rectangle_base, double rectangle_height)
+        {
+
+            try
+            {
+
+                double rectangle_area = rectangle_base * rectangle_height;
+
+                return $"El área del rectángulo con base {rectangle_base} y altura {rectangle_height} es: {rectangle_area}";
+
+            }
+            catch (Exception ex)
+            {
+                return $"Error al calcular el área del rectángulo: {ex.Message}";
+            }
+
+        }
+
+    }
+}
